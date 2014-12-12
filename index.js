b0c102f0a1ad0642a9a650557dfbf2c70dc24d6b
Dongfang (Tian) Mi <tianm@mit.edu>
2014-12-12 04:22:07 -0500

    setting up leaflet

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-shp("data").then(function(geojson){
-  console.log("stuff")
-  console.log(geojson);
+// shp("data").then(function(geojson){
+//   console.log("stuff")
+//   console.log(geojson);
+// });
 
-  var map = L.map('map');
-  L.geoJson(geojson).addTo(map);
-});
+var initMap = function() {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  // add a marker in the given location, attach some popup content to it and open the popup
+  L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+      .openPopup();
+}();
